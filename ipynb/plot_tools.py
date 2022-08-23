3ad75ea9ec7f8d7580956f06a011b7e5e7371fa9
Liwei Ji <jiliwei.phys@gmail.com>
2022-08-23 17:20:11 -0400

    add diff_dataset

--- ipynb/plot_tools.py
+++ ipynb/plot_tools.py
@@ -72,11 +72,34 @@ def interp_data(data, cols=[1,2], kind='linear'):
 def interp_dataset(dataset, cols=[1,2], kind='linear'):
     funcset = []
     for d in dataset:
-        funcrow=[]
+        funcrow = []
         funcrow.append(d[0])
         funcrow.append(interp_data(d[1], cols, kind))
         funcset.append(funcrow)
     return funcset
+
+
+############################################
+# Calculate Difference with Exact Solution #
+############################################
+def diff_data(f_data, f_exact, lims=[-0.5, 0.5], num=100):
+    diff = []
+    x = np.linspace(lims[0], lims[1], num)
+    for i in range(len(x)):
+        diffrow = []
+        diffrow.append(x[i])
+        diffrow.append(abs(f_data(x) - f_exact(x)))
+        diff.append(diffrow)
+    return diff
+
+def diff_dataset(f_set, f_exact, lims=[-0.5, 0.5], num=100):
+    diffset = []
+    for f in f_set:
+        diffrow = []
+        diffrow.append(f[0])
+        diffrow.append(diff_data(f[1], f_exact, lims, num))
+        diffset.append(diffrow)
+    return diffset
 
 
 ########
