d3c291321a0056702053817014489558c6cf806c
Liwei Ji <jiliwei.phys@gmail.com>
2022-08-23 16:04:34 -0400

    update plot_tools.py

--- ipynb/plot_tools.py
+++ ipynb/plot_tools.py
@@ -12,7 +12,15 @@ import matplotlib.pyplot as plt
 ##########
 
 # set limit for plt
-def set_limit(plt, lims):
+def set_plots(plt, labels, lims):
+    # set labels
+    if(labels):
+        if(type(labels)==list):
+            plt.xlabel(labels[0])
+            plt.ylabel(labels[1])
+        else:
+            plt.ylabel(labels)
+    # set limits
     if(lims):
         if(type(lims[0])==list):
             # 2d list case
@@ -39,9 +47,9 @@ def load_dataset(dirs, files):
     for d in dirs: # go over all dirs
         for f in os.listdir(d): # go over all files in d
             if re.search(files, f):
-                fdata = load_data(os.path.join(d,f))
+                fdata = load_data(os.path.join(d, f))
                 datarow = []
-                datarow.append(d)
+                datarow.append(os.path.basename(d)) # lowest level of dir
                 datarow.append(fdata)
                 dataset.append(datarow)
     return dataset
@@ -51,15 +59,14 @@ def load_dataset(dirs, files):
 # Plot #
 ########
 
-def plot_rho(dataset, marker='-', lims=False, savefig=False):
-    cols = [8, 11]
+def plot(dataset, cols=[1,2], marker='-',
+         labels=False, lims=False,
+         savefig=False):
     for d in dataset:
         plt.plot([i[cols[0]-1] for i in d[1]],
                  [i[cols[1]-1] for i in d[1]],
-                 marker, label=os.path.basename(d[0]))
-    plt.xlabel(r'x')
-    plt.ylabel(r'$\rho$')
-    set_limit(plt, lims)
+                 marker, label=d[0])
+    set_plots(plt, labels, lims)
     plt.legend(loc="best")
 
     if(savefig):
