dd3877fc4341eea161cb04fc1c8184b6e18c663d
Liwei Ji <jiliwei.phys@gmail.com>
2022-09-13 21:23:53 -0400

    beautify

--- ipynb/plots/plot_tools.py
+++ ipynb/plots/plot_tools.py
@@ -19,20 +19,19 @@ from scipy.integrate import simpson
 
 # interp 1d data
 def interp1d_data(data, cols=[1,2], kind='linear'):
-    x = [d[cols[0]-1] for d in data]
-    y = [d[cols[1]-1] for d in data]
-    return interp1d(x, y, kind)
+    return interp1d([d[cols[0]-1] for d in data],
+                    [d[cols[1]-1] for d in data], kind)
 
 # diff with exact solution
-def diff_data(data, f_ref, norm=1):
+def diff_data(data, f_exact):
     diff = []
-    #x = np.linspace(lims[0], lims[1], num)
     for i in range(2,len(data)-2):
-        diff.append([data[i,0], pow(abs(data[i,1]-f_ref(data[i,0])), norm)])
+        diff.append([data[i,0], abs(data[i,1]-f_exact(data[i,0]))])
     return diff
 
-def plt_exact(data, cols=[1,2], kind='linear', num=200, lims=[-0.5,0.5],
-             marker='-'):
+# plot interplated data
+def plt_exact(data, cols=[1,2], kind='linear', lims=[-0.5,0.5], num=200,
+              marker='-'):
     x_new = np.linspace(lims[0], lims[1], num)
     if (type(kind) == list):
         for i in range(len(kind)):
@@ -51,74 +50,59 @@ def plt_exact(data, cols=[1,2], kind='linear', num=200, lims=[-0.5,0.5],
 class DataSet(ds.DataSet):
     def __init__(self, dirs, files, cols=None):
         ds.DataSet.__init__(self, dirs, files, cols)
-        self.interp1dset = []
         self.diffset = []
-        self.integset = []
-        self.convorderset = []
-
-    # choose two cols in self.dataset to do 1d interp
-    def interp1d(self, cols=[1,2], kind='linear'):
-        self.interp1dset.clear()
-        for d in self.dataset:
-            self.interp1dset.append(interp1d_data(d, cols, kind))
-
-    def diff(self, f_ref, norm=1):
-        self.diffset.clear()
-        for d in self.dataset:
-            self.diffset.append(diff_data(d, f_ref, norm))
-
-    def integ(self):
-        self.integset.clear()
-        for d in self.diffset:
-            self.integset.append(simpson([x[1] for x in d], [x[0] for x in d]))
-
-    def convorder(self):
-        self.convorderset.clear()
-        for i in range(len(self.integset)-1):
-            low = self.integset[i]
-            high = self.integset[i+1]
-            self.convorderset.append(math.log(low/high, 2))
-
-    # wrapper
-    def calcAll(self, f_ref, norm=1):
-        self.diff(f_ref, norm)
-        self.integ()
-        self.convorder()
-
-    def getInterpSet(self):
-        return self.interp1dset
+        self.normset = []
+        self.convset = []
 
     def getDiffSet(self):
         return self.diffset
+    def getNormSet(self):
+        return self.normset
+    def getConvSet(self):
+        return self.convset
 
-    def getIntegSet(self):
-        return self.integset
+    def diff(self, f_exact):
+        self.diffset.clear()
+        for d in self.dataset:
+            self.diffset.append(diff_data(d, f_exact))
 
-    def getConvorderSet(self):
-        return self.convorderset
+    def norm(self, order=1):
+        self.normset.clear()
+        for d in self.diffset:
+            self.normset.append(pow(simpson([pow(x[1],order) for x in d],
+                                            [x[0] for x in d]), 1/order))
+
+    def conv(self):
+        self.convset.clear()
+        for i in range(len(self.normset)-1):
+            self.convset.append(math.log(self.normset[i]/self.normset[i+1], 2))
+
+    # wrapper
+    def calcAll(self, f_exact, order=1):
+        self.diff(f_exact)
+        self.norm(order)
+        self.conv()
 
     # plot functions
     def pltData(self, cols=[1,2], marker='-', labels=False, lims=False,
-                 fx=lambda x,i:x, fy=lambda x,i:x,
-                 savefig=False, fig_name='data.pdf',
-                 xlim=[-0.5,0.5], num=100, f_ref=None):
+                fx=lambda x,i:x, fy=lambda x,i:x,
+                savefig=False, fig_name='data.pdf',
+                num_ref=200, f_exact=None):
         ds.pltSet(self.dataset, self.dictset, cols, marker, labels, lims,
                   fx, fy)
-
-        if(f_ref is not None):
-            x_new = np.linspace(xlim[0], xlim[1], num)
-            plt.plot(x_new, f_ref(x_new), marker, label='exact')
-
+        if(f_exact is not None):
+            data0 = self.dataset[0]
+            x_new = np.linspace(data0[2,0], data0[len(data0)-3,0], num_ref)
+            plt.plot(x_new, f_exact(x_new), marker, label='exact')
         plt.legend(loc="best")
         if(savefig):
             plt.savefig(fig_name, bbox_inches='tight')
 
     def pltDiff(self, cols=[1,2], marker='-', labels=False, lims=False,
-                 fx=lambda x,i:x, fy=lambda x,i:x,
-                 savefig=False, fig_name='diff.pdf'):
+                fx=lambda x,i:x, fy=lambda x,i:x,
+                savefig=False, fig_name='diff.pdf'):
         ds.pltSet(self.diffset, self.dictset, cols, marker, labels, lims,
                   fx, fy)
-
         plt.legend(loc="best")
         if(savefig):
             plt.savefig(fig_name, bbox_inches='tight')
