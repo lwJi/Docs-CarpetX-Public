8152522f843f8fcc1996dc1ceda64342abdebb89
Liwei Ji <jiliwei.phys@gmail.com>
2022-10-11 15:22:32 -0400

    Update to adapt to new dataset.py

--- ipynb/plots/plot_tools.py
+++ ipynb/plots/plot_tools.py
@@ -30,16 +30,15 @@ def diff_data(data, f_exact):
     return diff
 
 # plot interplated data
-def plt_exact(data, cols=[1,2], kind='linear', lims=[-0.5,0.5], num=200,
-              marker='-'):
+def plt_exact(data, cols=[1,2], kind='linear', lims=[-0.5,0.5], num=200):
     x_new = np.linspace(lims[0], lims[1], num)
     if (type(kind) == list):
         for i in range(len(kind)):
             f_exact = interp1d_data(data, cols, kind[i])
-            plt.plot(x_new, f_exact(x_new), marker, label=kind[i])
+            plt.plot(x_new, f_exact(x_new), label=kind[i])
     else:
         f_exact = interp1d_data(data, cols, kind)
-        plt.plot(x_new, f_exact(x_new), marker, label=kind)
+        plt.plot(x_new, f_exact(x_new), label=kind)
     plt.legend(loc="best")
 
 
@@ -90,21 +89,20 @@ class DataSet(ds.DataSet):
         self.conv()
 
     # plot functions
-    def pltData(self, cols=[1,2], marker='-', fx=lambda x,i:x, fy=lambda x,i:x,
+    def pltData(self, cols=[1,2], fx=lambda x,i:x, fy=lambda x,i:x, markers='-',
                 savefig=False, fig_name='data.pdf', num_ref=200, f_exact=None):
-        ds.pltSet(self.dataset, self.dictset, cols, marker, fx, fy)
+        ds.pltSet(self.dataset, self.dictset, cols, fx, fy, markers)
         if(f_exact is not None):
             data0 = self.dataset[0]
             x_new = np.linspace(data0[2,0], data0[len(data0)-3,0], num_ref)
-            plt.plot(x_new, f_exact(x_new), marker, label='exact')
+            plt.plot(x_new, f_exact(x_new), label='exact')
         plt.legend(loc="best")
         if(savefig):
             plt.savefig(fig_name, bbox_inches='tight')
 
-    def pltDiff(self, cols=[1,2], marker='-', fx=lambda x,i:x, fy=lambda x,i:x,
+    def pltDiff(self, cols=[1,2], fx=lambda x,i:x, fy=lambda x,i:x, markers='-',
                 savefig=False, fig_name='diff.pdf'):
-        ds.pltSet(self.diffset, self.dictset, cols, marker,
-                  fx, fy)
+        ds.pltSet(self.diffset, self.dictset, cols, fx, fy, markers)
         plt.legend(loc="best")
         if(savefig):
             plt.savefig(fig_name, bbox_inches='tight')
